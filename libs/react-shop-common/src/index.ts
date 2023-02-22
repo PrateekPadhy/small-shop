5eb54074fdfc33c1c03f046556b1a330002f8671
Prateek Padhy <prateek@MacBook.local>
2023-02-22 00:04:40 +0000

    feat: custom theme

--- libs/react-shop-common/src/index.ts
+++ libs/react-shop-common/src/index.ts
@@ -1,5 +1,9 @@
+export * from './lib/components/header/header';
 export * from './lib/components/banner/banner';
 export * from './lib/react-shop-common';
 
 // Constants
 export * from './lib/constants';
+
+// Theme
+export * from './lib/themes/theme';
---
+++ libs/react-shop-common/src/lib/themes/theme.ts
@@ -0,0 +1,44 @@
+import { Color, createTheme, PaletteOptions } from '@mui/material';
+
+type ColorWithoutShades = Omit<Color, 'A100' | 'A200' | 'A400' | 'A700'>;
+
+const primary: ColorWithoutShades = {
+  50: '#f4e8e7', // brown-50 - same as primary.light
+  100: '#f2c9be', // brown-100
+  200: '#eaa695', // brown-200
+  300: '#e3856c', // brown-300
+  400: '#df6c4f', // brown-400 - same as primary.main
+  500: '#db5535', // brown-500
+  600: '#d14f31', // brown-600
+  700: '#c4492d', // brown-700
+  800: '#b54429', // brown-800
+  900: '#9d3821', // brown-900 - same as primary.dark
+};
+
+const secondary: ColorWithoutShades = {
+  50: '#e1f5f3', // teal-50 - same as primary.light
+  100: '#b3e6e0', // teal-100
+  200: '#81d6cc', // teal-200
+  300: '#49c5b7', // teal-300 - same as primary.main
+  400: '#0db7a6', // teal-400
+  500: '#00a995', // teal-500
+  600: '#009b87', // teal-600
+  700: '#008a77', // teal-700
+  800: '#007a68', // teal-800
+  900: '#005d4a', // teal-900 - same as primary.dark
+};
+
+const palette: PaletteOptions = {
+  primary: {
+    light: primary[50], // violet-50
+    main: primary[400], // violet-500
+    dark: primary[900], // violet-900
+  },
+  secondary: {
+    light: secondary[50], // red-50
+    main: secondary[300], // red-500
+    dark: secondary[900], // red-900
+  },
+};
+
+export const appTheme = createTheme({ palette });
